909f5173d20b537b2d1bbc68fcb31120fb10b9aa
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2016-06-26 23:41:51 -0600

    Initial commit

---
+++ irc.py
@@ -0,0 +1,12 @@
+class Connection:
+    def __init__(self, **kwargs):
+        print("Creating new IRC connection.")
+
+        nick = kwargs["nick"]
+        server = kwargs["server"]
+
+        print("nick: " + nick)
+        print("server: " + server)
+
+    def connect():
+
