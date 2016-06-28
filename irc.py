ec45052da1e5399ff83012ea01d851e2e4ff7d21
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2016-06-28 04:03:03 -0600

    Move majority of code into irc module

--- irc.py
+++ irc.py
@@ -1,17 +1,41 @@
 import socket
+import time
 
 import config
 
+BUFSIZE = 512
 
 class Connection:
     def __init__(self, **kwargs):
         print("Creating new IRC connection with the following configuration:")
-        print("nick: " + config.get('nick'))
-        print("server: " + config.get('server'))
+        print(open('config.yaml', 'r').read())
+
+        self.connect()
+
+        for channel in config.get('channels'):
+            self.connection.join_channel(channel)
+
+        while True:
+            message = self.connection.sock.recv(BUFSIZE)
+            if message:
+                self.parse_message(message)
+
+    def parse_message(self, message):
+        messages = message.split("\r\n")
+
+        for msg in messages:
+            if msg:
+                print(" [RECV] <-- " + repr(m))
 
     def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((config.get('server'), config.get('port')))
+        time.sleep(1)
 
     def join_channel(self, channel):
-        self.sock.send("JOIN " + channel)
+        print("Joining channel: " + channel)
+        self.send_message("JOIN " + channel)
+
+    def send_message(self, message):
+        print(" [SEND] --> " + repr(message))
+        self.sock.send(message + "\r\n")
