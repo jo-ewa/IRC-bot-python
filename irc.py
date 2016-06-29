7c5ad2a4e30a1d16f0bdb576765a3fbddaf00627
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2016-06-29 16:42:53 -0600

    But mostly functional and complete
    Bot stays connected, PONGs, parses messages, responds appropriately, and
    most initialization is complete for creating new IRC connections.

--- irc.py
+++ irc.py
@@ -1,52 +1,110 @@
+import string
 import socket
 import time
+import datetime
 import re
+import sys
 
 import config
 
 BUFSIZE = 512
 
 class Connection:
-    def __init__(self, **kwargs):
-        print("Creating new IRC connection with the following configuration:")
-        print(open('config.yaml', 'r').read())
+    def identify_with_nickserv(self):
+        self.send_message("PRIVMSG NickServ IDENTIFY %s %s" % (config.get("nick"), config.get("password")))
 
-        self.connect()
-
-        self.send_message("NICK " + config.get('nick'))
-        self.send_message("USER " + config.get('nick') + " ")
-
-        for channel in config.get('channels'):
-            self.join_channel(channel)
-
-        while True:
-            data = self.sock.recv(BUFSIZE)
-            if data:
-                self.parse_data(data)
-
-    def parse_data(self, data):
-        messages = data.split("\r\n")
-
+    def parse_messages(self, messages):
         for message in messages:
-            if message:
-                print(" [R] <-  " + repr(message))
+            if re.match(r"^PING .*", message):
+                self.pong(message)
+                break
+            if re.search(r".*001 %s.*" % config.get("nick"), message):
+                self.join_config_channels()
+                break
+            if re.match(r".*NOTICE.*nickname.*registered.*", message):
+                self.identify_with_nickserv()
+                break
+            if not self.registered:
+                self.register_nick_and_username()
+                self.registered = True
 
-                if re.match(r"^PING .*", message):
-                    self.pong(message)
                 
-
-    def connect(self):
+    def create_socket(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((config.get('server'), config.get('port')))
         time.sleep(1)
+
+    def log_messages(self, messages, received=False):
+        if type(messages) is str:
+            messages = [messages]
+
+        for message in messages:
+            log_message = []
+            if received:
+                log_message.append(" [RECEIVED] - ")
+            else:
+                log_message.append(" [SENT]     - ")
+            log_message.append(datetime.datetime.now().isoformat())
+            log_message.append(": ")
+            log_message.append(message)
+            print(string.join(log_message))
+
+    def join_config_channels(self):
+        for channel in config.get("channels"):
+            self.join_channel(channel)
 
     def join_channel(self, channel):
         self.send_message("JOIN " + channel)
 
     def send_message(self, message):
-        print(" [S]  -> " + repr(message))
+        self.log_messages(message)
         self.sock.send(message + "\r\n")
 
     def pong(self, message):
         ping_id = re.match(r"PING (.*)", message).group(1)
         self.send_message("PONG " + ping_id)
+
+    def receive_messages(self):
+        raw = self.sock.recv(BUFSIZE)
+
+        messages = filter(None, raw.split("\r\n"))
+
+        self.log_messages(messages, True)
+
+        return messages
+
+    def receive_all(self):
+        messages = self.receive_messages()
+        self.parse_messages(messages)
+        while messages:
+            messages = self.receive_messages()
+            self.parse_messages(messages)
+
+    def register_nick_and_username(self):
+        self.send_message(string.join([
+            "NICK",
+            config.get("nick")], " "
+        ))
+
+        self.send_message(string.join([
+            "USER",
+            config.get("nick"),
+            socket.gethostname(),
+            config.get("server"),
+            config.get("nick")], " "
+        ))
+
+
+    def start_loop(self):
+        while True:
+            self.parse_messages(self.receive_all())
+
+    def __init__(self, **kwargs):
+        print("Creating new IRC connection. YAML Configuration:")
+        print(open('config.yaml', 'r').read() + "\n")
+
+        self.registered = False
+
+        self.create_socket()
+
+        self.start_loop()
