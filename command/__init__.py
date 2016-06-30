5ee0b06a0b8f2e23481d94c5c4d4cf57b8695cca
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2016-06-30 02:53:17 -0600

    Add error handling for unknown commands

--- command/__init__.py
+++ command/__init__.py
@@ -3,5 +3,8 @@ import string
 import importlib
 
 def execute(channel, command, arguments, connection):
-    module = importlib.import_module(string.join([__name__, command], '.'))
-    module.execute(arguments, channel, connection)
+    try:
+        module = importlib.import_module(string.join([__name__, command], '.'))
+        module.execute(arguments, channel, connection)
+    except ImportError:
+        connection.send_to_channel(channel, "No such command.")
--- irc.py
+++ irc.py
@@ -8,54 +8,66 @@ import sys
 import config
 import command
 
+def log_messages(messages, received=False):
+    if type(messages) is str:
+        messages = [messages]
+
+    for message in messages:
+        log_message = []
+        if received:
+            log_message.append(" [RECEIVED] - ")
+        else:
+            log_message.append(" [SENT]     - ")
+        log_message.append(datetime.datetime.now().isoformat())
+        log_message.append(": ")
+        log_message.append(message)
+        print(string.join(log_message))
+
 BUFSIZE = 512
 
-class Connection:
+COMMANDS = {
+    'NICKSERV_IDENTIFY': "PRIVMSG NickServ IDENTIFY %s %s",
+}
+
+PARSE_REGEX = {
+    'BOT_COMMAND': r".*PRIVMSG ([^\s]+) :%s([\w]+)[\s]*(.*)",
+    'PING': r"^PING .*",
+    'RPL_WELCOME': r"001 %s",
+    'NICK_REGISTERED': r"NOTICE.*nickname.*registered",
+}
+
+class ServerConnection:
     def identify_with_nickserv(self):
-        self.send_message("PRIVMSG NickServ IDENTIFY %s %s" % (config.get("nick"), config.get("password")))
+        self.send_message(
+            COMMANDS['NICKSERV_IDENTIFY'] % (config.get("nick"), config.get("password"))
+        )
 
     def parse_messages(self, messages):
         for message in messages:
-            result = re.match(r".*PRIVMSG ([^\s]+) :%s([\w]+)[\s]*(.*)" % config.get("command_prefix"), message)
+            result = re.match(PARSE_REGEX['BOT_COMMAND'] % config.get("command_prefix"), message)
             if result:
                 command.execute(result.group(1), result.group(2), result.group(3), self)
                 break
-            if command.register.check(message):
-                command.register.trigger(message)
-                break
-            if re.match(r"^PING .*", message):
+            #if command.register.check(message):
+                #command.register.trigger(message)
+                #break
+            if re.match(PARSE_REGEX['PING'], message):
                 self.pong(message)
                 break
-            if re.search(r"001 %s" % config.get("nick"), message):
+            if re.search(PARSE_REGEX['RPL_WELCOME'] % config.get("nick"), message):
                 self.join_config_channels()
                 break
-            if re.search(r"NOTICE.*nickname.*registered", message):
+            if re.search(PARSE_REGEX['NICK_REGISTERED'], message):
                 self.identify_with_nickserv()
                 break
             if not self.registered:
                 self.register_nick_and_username()
                 self.registered = True
-
                 
     def create_socket(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((config.get('server'), config.get('port')))
         time.sleep(1)
-
-    def log_messages(self, messages, received=False):
-        if type(messages) is str:
-            messages = [messages]
-
-        for message in messages:
-            log_message = []
-            if received:
-                log_message.append(" [RECEIVED] - ")
-            else:
-                log_message.append(" [SENT]     - ")
-            log_message.append(datetime.datetime.now().isoformat())
-            log_message.append(": ")
-            log_message.append(message)
-            print(string.join(log_message))
 
     def join_config_channels(self):
         for channel in config.get("channels"):
@@ -64,8 +76,11 @@ class Connection:
     def join_channel(self, channel):
         self.send_message("JOIN " + channel)
 
+    def send_to_channel(self, channel, message):
+        self.send_message("PRIVMSG %s %s" % (channel, message))
+
     def send_message(self, message):
-        self.log_messages(message)
+        log_messages(message)
         self.sock.send(message + "\r\n")
 
     def pong(self, message):
@@ -77,7 +92,7 @@ class Connection:
 
         messages = filter(None, raw.split("\r\n"))
 
-        self.log_messages(messages, True)
+        log_messages(messages, True)
 
         return messages
 
