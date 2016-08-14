506203d003861dc530bd800182bf600b8362515a
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2016-08-14 02:01:20 -0600

    Re-indent code

--- nickenbot/irc.py
+++ nickenbot/irc.py
@@ -2,36 +2,35 @@ COMMANDS = {
     'NICKSERV_IDENTIFY': "PRIVMSG NickServ IDENTIFY %s %s",
 }
 
-    def identify_with_nickserv(self):
-        self.send_message(
-            COMMANDS['NICKSERV_IDENTIFY'] % (config.get("nick"), config.get("password"))
-        )
+def identify_with_nickserv(servconn):
+    servconn.send_message(
+        COMMANDS['NICKSERV_IDENTIFY'] % (config.get("nick"), config.get("password"))
+    )
 
-    def join_config_channels(self):
-        for channel in config.get("channels"):
-            self.join_channel(channel)
+def join_config_channels(servconn):
+    for channel in config.get("channels"):
+        servconn.join_channel(channel)
 
-    def join_channel(self, channel):
-        self.send_message("JOIN " + channel)
+def join_channel(servconn, channel):
+    servconn.send_message("JOIN " + channel)
 
-    def send_to_channel(self, channel, message):
-        self.send_message("PRIVMSG %s %s" % (channel, message))
+def send_to_channel(servconn, channel, message):
+    servconn.send_message("PRIVMSG %s %s" % (channel, message))
 
-    def pong(self, message):
-        ping_id = re.match(r"PING (.*)", message).group(1)
-        self.send_message("PONG " + ping_id, False)
+def pong(servconn, message):
+    ping_id = re.match(r"PING (.*)", message).group(1)
+    servconn.send_message("PONG " + ping_id, False)
 
-    def register_nick_and_username(self):
-        self.send_message(string.join([
-            "NICK",
-            config.get("nick")], " "
-        ))
+def register_nick_and_username(servconn):
+    servconn.send_message(string.join([
+        "NICK",
+        config.get("nick")], " "
+    ))
 
-        self.send_message(string.join([
-            "USER",
-            config.get("nick"),
-            socket.gethostname(),
-            config.get("server"),
-            config.get("nick")], " "
-        ))
-
+    servconn.send_message(string.join([
+        "USER",
+        config.get("nick"),
+        socket.gethostname(),
+        config.get("server"),
+        config.get("nick")], " "
+    ))
