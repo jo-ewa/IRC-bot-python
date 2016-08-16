df067dcdbd43777dccad896939d6f07d24a38330
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2016-08-15 19:31:10 -0600

    Minor update to about text

--- nickenbot/command/about.py
+++ nickenbot/command/about.py
@@ -1,6 +1,6 @@
 from .. import irc
 
-about_text = "A simple bot written in Python by nickenchuggets. Source: https://github.com/brlafreniere/nickenbot"
+about_text = "A simple bot written in Python. Source: https://github.com/brlafreniere/nickenbot"
 
 def execute(**kwargs):
     irc.send_to_channel(kwargs['channel'], about_text)
