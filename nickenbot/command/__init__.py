cba45e5c7e65c21d75e51a8ef1121547d78daaff
jo-ewa <11009431+jo-ewa@users.noreply.github.com>
2016-08-15 18:42:01 -0600

    Add support for hyphens, and list of commands
    Adds a function to retrieve all commands, and converts incoming commands
    from hyphenated to underscored form.

--- nickenbot/command/__init__.py
+++ nickenbot/command/__init__.py
@@ -1,11 +1,22 @@
 import os
+import fnmatch
 import string
 import importlib
 import traceback
 from .. import irc
 
+def get_all():
+    files = os.listdir('./nickenbot/command')
+    files.remove('__init__.py')
+    commands = [os.path.splitext(f)[0] for f in files if fnmatch.fnmatch(f, '*.py')]
+    commands = [string.replace(c, '_', '-') for c in commands]
+    return commands
+
 def execute(**kwargs):
-    module_string = string.join([__name__, kwargs['command']], '.')
+    print(kwargs['command'])
+    command = string.replace(kwargs['command'], '-', '_')
+    print(command)
+    module_string = string.join([__name__, command], '.')
 
     module = None
     try:
